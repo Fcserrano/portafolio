97e0b45257303b05e11333195f2026c3cba0d638
Fernando César Serrano <78665732+Fcserrano@users.noreply.github.com>
2022-03-24 16:23:40 -0300

    Add files via upload

---
+++ enviarCorreo.php
@@ -0,0 +1,12 @@
+<?php
+$destino = "fernando.cesar.serrano1997@gmail.com";
+
+$email = $_POST['email'];
+$asunto = $_POST['subject'];
+$mensaje = $_POST['message'];
+
+$header = "Enviado desde el formulario del portafolio";
+$mensajeCompleto = "\n email: " .$email . "\n" . "asunto: " 
+            . $asunto . "\n" . "mensaje: " . $mensaje;
+
+mail($destino, $asunto, $mensajeCompleto, $header);
